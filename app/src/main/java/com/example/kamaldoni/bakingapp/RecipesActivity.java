677dda7124222a3f4c1fb34cdedfcb4e8cabf162
Daniyar <kamal.daniyar.96@gmail.com>
2018-05-26 07:42:10 +0900

    Make first commit

---
+++ app/src/main/java/com/example/kamaldoni/bakingapp/RecipesActivity.java
@@ -0,0 +1,19 @@
+package com.example.kamaldoni.bakingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+public class RecipesActivity extends AppCompatActivity {
+    private RecyclerView recipeCards;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes);
+
+
+
+
+    }
+}
